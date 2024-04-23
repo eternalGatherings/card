f4225f153b82323fcd5583da6b47db2527d4730f
sachinknsachi <sachinknsachi@gmail.com>
2024-04-23 09:23:33 +0530

    Added About to business card

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,11 +10,24 @@ function Main() {
       <div className="name-designation">
         <span className="name">Sachin Kn</span>
         <span className="designation">Full Stack Developer</span>
-        <a className="website" href="https://www.sachinkn.in/" target="_blank" rel="noreferrer noopener">www.sachinkn.in</a>
+        <a
+          className="website"
+          href="https://www.sachinkn.in/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          www.sachinkn.in
+        </a>
         <div className="buttons">
           <button className="email-button">
             <img src={emailIcon} className="email-icon" alt="Email Icon" />
-            <a href="mailto:sachinkn.official@gmail.com" target="_blank" rel="noreferrer noopener">Email</a>
+            <a
+              href="mailto:sachinkn.official@gmail.com"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Email
+            </a>
           </button>
           <button className="linkedin-button">
             <img
@@ -22,7 +35,13 @@ function Main() {
               className="linkedin-icon"
               alt="LinkedIn Icon"
             />
-            <a href="https://www.linkedin.com/in/sachin-kn/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+            <a
+              href="https://www.linkedin.com/in/sachin-kn/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              LinkedIn
+            </a>
           </button>
         </div>
       </div>
@@ -30,13 +49,21 @@ function Main() {
         <div className="about">
           <div className="header">About</div>
           <div className="paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
-            corrupti illum harum vel vitae incidunt iure alias, ratione quos nam
-            asperiores rerum accusantium, nobis, aut amet vero a? Reprehenderit,
-            corporis.
+            I am a dedicated and results-driven software development and testing
+            professional with over five years of industry experience. I have a
+            strong technical proficiency in front-end and back-end technologies
+            including HTML, CSS, JavaScript, Java, and Python, with
+            specializations in React.js, Angular.js, Node.js, Express.js, and
+            MongoDB. My expertise extends to testing frameworks such as
+            Playwright, Mocha, Jest, Chai, Selenium, TestNG, and REST Assured
+            for API testing.<br/><br/>I excel in problem-solving and committed to continuous
+            learning and professional development. Actively seeking
+            opportunities in a dynamic work environment where I can effectively
+            contribute to projects and initiatives, while advancing my career in
+            software development and quality assurance.
           </div>
         </div>
-        <div className="interests">
+        {/* <div className="interests">
           <div className="header">Interests</div>
           <div className="paragraph">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
@@ -44,7 +71,7 @@ function Main() {
             asperiores rerum accusantium, nobis, aut amet vero a? Reprehenderit,
             corporis.
           </div>
-        </div>
+        </div> */}
       </div>
     </main>
   );
