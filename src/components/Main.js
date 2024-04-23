1eccabe191109da141c4078633243bd41de91fc9
sachinknsachi <sachinknsachi@gmail.com>
2024-04-23 09:50:22 +0530

    modified About to business card

--- src/components/Main.js
+++ src/components/Main.js
@@ -52,11 +52,13 @@ function Main() {
             I am a dedicated and results-driven software development and testing
             professional with over five years of industry experience. I have a
             strong technical proficiency in front-end and back-end technologies
-            including HTML, CSS, JavaScript, Java, and Python, with
+            including HTML, CSS, JavaScript, Java, Python and SQL, with
             specializations in React.js, Angular.js, Node.js, Express.js, and
             MongoDB. My expertise extends to testing frameworks such as
-            Playwright, Mocha, Jest, Chai, Selenium, TestNG, and REST Assured
-            for API testing.<br/><br/>I excel in problem-solving and committed to continuous
+            Playwright, Mocha, Jest, Chai, Selenium, TestNG, and REST Assured.
+            <br/>
+            <br/>
+            I excel in problem-solving and committed to continuous
             learning and professional development. Actively seeking
             opportunities in a dynamic work environment where I can effectively
             contribute to projects and initiatives, while advancing my career in
